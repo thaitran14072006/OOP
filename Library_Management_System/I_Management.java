d39e946972fa8b66b23cb2129f3fa56de0c67f1b
tranthai14072006 <tranthai91thcs@gmail.com>
2025-11-13 00:37:10 +0700

    delete

--- Library_Management_System/I_Management.java
+++
@@ -1,8 +0,0 @@
-public interface I_Management<T> {
-    void add(T item);
-    void remove(String id);
-    T search(String id);
-    void editProfile(String id);
-    void show(String id);
-    void showAll();
-}
